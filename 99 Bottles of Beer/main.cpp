cb2a8ed696faab42222dceb22a298fd1dfb7285a
nas5690 <nsiddiqui590@gmail.com>
2016-09-01 16:52:18 -0500

    Modified a bit.

--- 99 Bottles of Beer/main.cpp
+++ 99 Bottles of Beer/main.cpp
@@ -1,4 +1,4 @@
-/*Coding the 99 Bottles of Beer Song*/
+/*Coding the 99 Bottles of Coke Song*/
 
 #include <iostream>
 #include <cmath>
@@ -11,25 +11,25 @@ int main()
 
 	for (int bottles = 99; bottles > 2; bottles--)
 	{
-		cout << "\n\t\t\t" << bottles << " bottles of beer on the wall, " << bottles << " bottles of beer.";
+		cout << "\n\t\t\t" << bottles << " bottles of coke on the wall, " << bottles << " bottles of coke.";
 		cout << "\n\t\t\t Take one down, pass it around, ";
-		cout << bottles - 1 << "\n\t\t\t bottles of beer on the wall." << endl;
+		cout << bottles - 1 << "\n\t\t\t bottles of coke on the wall." << endl;
 	} 
 	if (int bottles = 2)
 	{
-		cout << "\n\t\t\t 2 bottles of beer on the wall, 2 bottles of beer.";
+		cout << "\n\t\t\t 2 bottles of coke on the wall, 2 bottles of coke.";
 		cout << "\n\t\t\t Take one down, pass it around, ";
-		cout << "\n\t\t\t 1 more bottle of beer on the wall.";
+		cout << "\n\t\t\t 1 more bottle of coke on the wall.";
 		cout << "\n\n";
 	}
 	
-	cout << "\n\t\t\t Last bottle of beer on the wall, last bottle of beer.";
+	cout << "\n\t\t\t Last bottle of coke on the wall, last bottle of coke.";
 	cout << "\n\t\t\t Take it down, pass it around, ";
-	cout << "\n\t\t\t no more bottles of beer on the wall.";
+	cout << "\n\t\t\t no more bottles of coke on the wall.";
 	cout << "\n\n" << endl;
 
 	system("PAUSE");
-	cout << "\n\t\t\t You're all out. GET MORE BEER DAMMIT!!!" << endl;
+	cout << "\n\t\t\t You're all out. More Coca-Cola please!!!" << endl;
 
 	system("PAUSE");
 	return 0;
